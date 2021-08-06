b0dd432ef9ea6d0f4c90f524cc9ff1ecdd361789
Daniel Poss <dpatposs@msn.com>
2021-08-06 06:35:15 -0500

    - Added a Clash-Bot service to the UI - Removed Karma - Added Jest - Updated to use Angular Materials - TODO - need to add disclaimer for Angular Materials

--- ClashBot-UI/src/app/app.component.ts
+++ ClashBot-UI/src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ClashTeam} from "./clash-team";
+import {ClashBotService} from "./clash-bot.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'Clash-Bot';
+  teams: ClashTeam[] = [
+    {
+      teamName: 'Team Abra',
+      playersDetails: [
+        {
+          name: 'Roïdräge',
+          champions: ['Volibear', 'Ornn', 'Sett'],
+          role: 'Top'
+        },
+        {
+          name: 'TheIncentive',
+          champions: ['Lucian'],
+          role: 'ADC'
+        },
+        {
+          name: 'Pepe Conrad',
+          champions: ['Lucian'],
+          role: 'Jg'
+        }
+      ]
+    }
+  ];
+  clashServiceSubscription: Subscription | undefined;
+
+
+  constructor(private clashBotService: ClashBotService) {
+  }
+
+  ngOnInit(): void {
+    this.clashServiceSubscription = this.clashBotService
+      .getClashTeams()
+      .subscribe(data => {
+        this.teams = data;
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.clashServiceSubscription) {
+      this.clashServiceSubscription.unsubscribe();
+    }
+  }
+
 }
--- ClashBot-UI/src/app/app.module.ts
+++ ClashBot-UI/src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {TeamCardComponent} from './team-card/team-card.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ClashBotService} from "./clash-bot.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
-  providers: [],
+  providers: [ClashBotService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
