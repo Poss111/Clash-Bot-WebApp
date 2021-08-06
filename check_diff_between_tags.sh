829945f794881605ea055cb456c39c001b4d5fd8
Daniel Poss <dpatposs@msn.com>
2021-08-06 10:38:11 -0500

    - Removing individual deployment scripts - Adding single script to test diff script - Added diff script to check diff between tags

--- check_diff_between_tags.sh
+++ check_diff_between_tags.sh
@@ -4,13 +4,18 @@ serviceUpdate=false
 uiUpdate=false
 for i in $(git diff --name-only $(git tag --sort version:refname | tail -n 2 | head -n 1) $(git tag --sort version:refname | tail -n 1))
 do
-  if [[ "$i" == *"ClashBot-Service/"* ]];
+  if [[ "$i" == *"ClashBot-Service/"*  && !$serviceUpdate ]];
   then
     serviceUpdate=true
   fi
+  if [[ "$i" == *"ClashBot-UI/"* && !$uiUpdate ]];
+  then
+    uiUpdate=true
+  fi
 done
 
-echo "$serviceUpdate"
+echo "::set-output name=serviceUpdate::$serviceUpdate"
+echo "::set-output name=uiUpdate::$uiUpdate"
 echo "Finished"
 
 
