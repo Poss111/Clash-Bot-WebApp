21bb00d01c2eb2dd9322ed7075e687ea4a8b8dbe
Daniel Poss <dpatposs@msn.com>
2021-08-06 09:22:30 -0500

    - Adding base Dockerfiles for ease of local development - Adding build to differentiate between applications needing to be built - Added nodemon to service

--- ClashBot-Service/index.js
+++ ClashBot-Service/index.js
@@ -1,9 +1,61 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-    res.send('Up and running!');
+app.use(cors())
+
+app.get('/teams', (req, res) => {
+    res.send([
+        {
+            teamName: 'Team Abra',
+            tournamentDetails: {
+                tournamentName: 'Bandle City',
+                tournamentDay: '1'
+            },
+            playersDetails: [
+                {
+                    name: 'Roïdräge',
+                    champions: ['Volibear', 'Ornn', 'Sett'],
+                    role: 'Top'
+                },
+                {
+                    name: 'TheIncentive',
+                    champions: ['Lucian'],
+                    role: 'ADC'
+                },
+                {
+                    name: 'Pepe Conrad',
+                    champions: ['Lucian'],
+                    role: 'Jg'
+                }
+            ]
+        },
+        {
+            teamName: 'Team Behaviour',
+            tournamentDetails: {
+                tournamentName: 'Bandle City',
+                tournamentDay: '2'
+            },
+            playersDetails: [
+                {
+                    name: 'Roïdräge',
+                    champions: ['Volibear', 'Ornn', 'Sett'],
+                    role: 'Top'
+                },
+                {
+                    name: 'TheIncentive',
+                    champions: ['Lucian'],
+                    role: 'ADC'
+                },
+                {
+                    name: 'Pepe Conrad',
+                    champions: ['Lucian'],
+                    role: 'Jg'
+                }
+            ]
+        }
+    ]);
 })
 
 app.listen(port, () => {
