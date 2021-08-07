325b58d0bf4189cb83fd75bd91ecaaa9a3254859
Daniel Poss <dpatposs@msn.com>
2021-08-07 08:05:17 -0500

    - Swapping path of dist folder

--- ClashBot-UI/src/app/app.component.ts
+++ ClashBot-UI/src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit, OnDestroy {
   title = 'Clash-Bot';
   teams: ClashTeam[] = [
     {
-      teamName: 'Team Squirtle',
+      teamName: 'Team Blastoise',
       playersDetails: [
         {
           name: 'Roïdräge',
