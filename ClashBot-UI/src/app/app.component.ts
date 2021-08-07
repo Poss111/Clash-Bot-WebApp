8704fdd1d1ca71c138ae08f647ae2e792aed3396
Daniel Poss <dpatposs@msn.com>
2021-08-07 12:53:07 -0500

    - Set the cache control upon copy to s3 - Updated app.component.ts to test

--- ClashBot-UI/src/app/app.component.ts
+++ ClashBot-UI/src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit, OnDestroy {
   title = 'Clash-Bot';
   teams: ClashTeam[] = [
     {
-      teamName: 'Team Gottem',
+      teamName: 'Team Got',
       playersDetails: [
         {
           name: 'Roïdräge',
