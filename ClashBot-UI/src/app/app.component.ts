7409bb4868a33121997e31d6d2e3557b64efa6d4
Daniel Poss <dpatposs@msn.com>
2021-08-07 07:54:58 -0500

    - Adding failure if dist folder is not found

--- ClashBot-UI/src/app/app.component.ts
+++ ClashBot-UI/src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit, OnDestroy {
   title = 'Clash-Bot';
   teams: ClashTeam[] = [
     {
-      teamName: 'Team Charizard',
+      teamName: 'Team Pikachu',
       playersDetails: [
         {
           name: 'Roïdräge',
