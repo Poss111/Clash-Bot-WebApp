0b095386c1180f9c25c6ce4f94a4330061877747
Daniel Poss <dpatposs@msn.com>
2021-08-07 07:56:38 -0500

    - removing working-directory from upload-artifact

--- ClashBot-UI/src/app/app.component.ts
+++ ClashBot-UI/src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit, OnDestroy {
   title = 'Clash-Bot';
   teams: ClashTeam[] = [
     {
-      teamName: 'Team Pikachu',
+      teamName: 'Team Bulbasaur',
       playersDetails: [
         {
           name: 'Roïdräge',
