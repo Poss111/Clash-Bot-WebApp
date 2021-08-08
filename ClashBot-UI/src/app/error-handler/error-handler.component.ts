dbd920951213e32c827b097081317c98e2319f01
Daniel Poss <dpatposs@msn.com>
2021-08-08 06:35:23 -0500

    - UI - Adding error component

---
+++ ClashBot-UI/src/app/error-handler/error-handler.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error-handler',
+  templateUrl: './error-handler.component.html',
+  styleUrls: ['./error-handler.component.scss']
+})
+export class ErrorHandlerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
