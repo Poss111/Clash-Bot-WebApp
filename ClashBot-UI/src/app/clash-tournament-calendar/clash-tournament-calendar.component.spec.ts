7140f4aef42200ef2afc93f663b5863891d464c4
Daniel Poss <dpatposs@msn.com>
2021-08-12 10:58:21 -0500

    - UI - Added test cases for Clash Tournament Calendar component

--- ClashBot-UI/src/app/clash-tournament-calendar/clash-tournament-calendar.component.spec.ts
+++ ClashBot-UI/src/app/clash-tournament-calendar/clash-tournament-calendar.component.spec.ts
@@ -1,6 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ClashTournamentCalendarComponent} from './clash-tournament-calendar.component';
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatCardModule} from "@angular/material/card";
+import {NgModule} from "@angular/core";
+import {ClashTournamentCalendarHeaderComponent} from "../clash-tournament-calendar-header/clash-tournament-calendar-header.component";
 
-import { ClashTournamentCalendarComponent } from './clash-tournament-calendar.component';
+
+@NgModule({
+  declarations: [ClashTournamentCalendarHeaderComponent],
+  entryComponents: [ClashTournamentCalendarHeaderComponent]
+})
+class ClashTournamentCalendarHeaderTestModule {
+}
 
 describe('ClashTournamentCalendarComponent', () => {
   let component: ClashTournamentCalendarComponent;
@@ -8,7 +20,8 @@ describe('ClashTournamentCalendarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ClashTournamentCalendarComponent ]
+      declarations: [ ClashTournamentCalendarComponent ],
+      imports: [MatCardModule, MatDatepickerModule, MatNativeDateModule, ClashTournamentCalendarHeaderTestModule]
     })
     .compileComponents();
   });
@@ -19,7 +32,34 @@ describe('ClashTournamentCalendarComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  test('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  test('Is Selected function should return true if the data passed to it matches one of the days given as an input.', () => {
+    let mockDaysSelect = [];
+    let currentDate = new Date();
+    let tomorrowDate = new Date();
+    tomorrowDate.setDate(currentDate.getDate() + 1);
+    mockDaysSelect.push(currentDate);
+    mockDaysSelect.push(tomorrowDate);
+    component.daysSelected = mockDaysSelect;
+    let yesterday = new Date();
+    yesterday.setDate(currentDate.getDate() - 1);
+    let twoDaysAhead = new Date();
+    twoDaysAhead.setDate(currentDate.getDate() + 2);
+    expect(component.isSelected(yesterday)).toEqual('notselected');
+    expect(component.isSelected(twoDaysAhead)).toEqual('notselected');
+    expect(component.isSelected(currentDate)).toEqual('selected');
+    expect(component.isSelected(tomorrowDate)).toEqual('selected');
+  })
+
+  test('The minimum date for the calendar should be set to today.', () => {
+    expect(component.minDate).toEqual(new Date());
+  })
+
+  test('The calendar header component should be of instance ClashTournamentCalendarHeaderComponent', () => {
+    expect(component.calendarHeaderComponent instanceof ClashTournamentCalendarHeaderComponent).toBeTruthy();
+  })
+
 });
