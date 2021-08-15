42e62e7e0f17bc09f09f185a4ea4544518fd0c1d
Poss111 <dpatposs@msn.com>
2021-08-15 18:22:28 -0500

    - Adding docker compose file to setup service layer

--- ClashBot-Service/dao/impl/dynamo-db-helper.js
+++ ClashBot-Service/dao/impl/dynamo-db-helper.js
@@ -9,11 +9,16 @@ class DynamoDbHelper {
             if (!this.setupConfig) {
                 if (process.env.INTEGRATION_TEST) {
                     console.log('Loading credentials for Integration Test.');
+                    if (!process.env.HOST) {
+                        process.env.HOST = 'localhost';
+                    }
+                    console.log(`HOST : ${process.env.HOST}`);
                     dynamodb.AWS.config.update({
                         region: `${process.env.REGION}`,
-                        endpoint: 'http://localhost:8000',
+                        endpoint: `http://${process.env.HOST}:8000`,
                         accessKeyId: 'Dummy',
                         secretAccessKey: 'Dummy',
+                        maxRetries: 0,
                         httpOptions: {
                             connectTimeout: 2000,
                             timeout: 2000
