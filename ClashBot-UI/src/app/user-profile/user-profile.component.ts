03c7bec8248a53cd0a3cbb69a4fe42fa1d2d51ed
Daniel Poss <dpatposs@msn.com>
2021-08-20 12:12:32 -0500

    - UI - User Profile - Added Test cases and error handling

--- ClashBot-UI/src/app/user-profile/user-profile.component.ts
+++ ClashBot-UI/src/app/user-profile/user-profile.component.ts
@@ -11,6 +11,7 @@ import {UserDetailsService} from "../user-details.service";
 import {DiscordService} from "../discord.service";
 import {DiscordGuild} from "../discord-guild";
 import {UserDetails} from "../user-details";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-user-profile',
@@ -19,12 +20,11 @@ import {UserDetails} from "../user-details";
 })
 export class UserProfileComponent implements OnInit {
 
-  username?: string;
   selectable = true;
   removable = true;
   separatorKeysCodes: number[] = [ENTER, COMMA];
   championAutoCompleteCtrl = new FormControl();
-  filteredChampions: Observable<string[]> = of();
+  championsAutofillArray: Observable<string[]> = of();
   preferredChampions: Set<string> = new Set<string>();
   listOfChampions: string[] = [];
   initialFormControlState: any = {};
@@ -39,7 +39,8 @@ export class UserProfileComponent implements OnInit {
   constructor(private discordService: DiscordService,
               private clashBotService: ClashBotService,
               private userDetailsService: UserDetailsService,
-              private riotDdragonService: RiotDdragonService) {
+              private riotDdragonService: RiotDdragonService,
+              private matSnackBar: MatSnackBar) {
   }
 
   notInListValidator(): ValidatorFn {
@@ -50,32 +51,64 @@ export class UserProfileComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.discordService.getGuilds().subscribe((guilds) => {
+    this.discordService.getGuilds()
+      .pipe(take(1),
+        timeout(4000),
+        catchError((err) => {
+          console.error(err);
+          this.matSnackBar.open('Oops! Failed to load your guild information.', 'X', {duration: 5000});
+          return throwError(err);
+        })).subscribe((guilds) => {
       guilds.forEach(guild => {
         this.guilds.push(guild);
       });
       this.userDetailsService.getUserDetails().pipe(take(1)).subscribe((userDetails) => {
-        this.userDetails = userDetails;
-        this.clashBotService.getUserDetails(userDetails.id).pipe(take(1)).subscribe((data: ClashBotUserDetails) => {
-          this.defaultGuild = data.serverName;
-          let preferredChampions = Array.isArray(data.preferredChampions) ? data.preferredChampions : [];
-          this.riotDdragonService.getListOfChampions().pipe(take(1)).subscribe((championData) => {
-            this.listOfChampions = Object.keys(championData.data);
-            this.listOfChampions = this.listOfChampions.filter(record => !preferredChampions.includes(record));
-            this.initialAutoCompleteArray = JSON.parse(JSON.stringify(this.listOfChampions));
-          })
-
-          this.userDetailsForm = new FormGroup({
-            preferredChampionsFC: new FormControl([...preferredChampions]),
-            subscribedDiscordDMFC: new FormControl(data.subscriptions.UpcomingClashTournamentDiscordDM),
-            defaultGuildFC: new FormControl(this.defaultGuild)
-          });
-          this.preferredChampions = new Set<string>(data.preferredChampions);
-          this.initialFormControlState = JSON.parse(JSON.stringify(this.userDetailsForm.value));
-          this.filteredChampions = this.championAutoCompleteCtrl.valueChanges.pipe(
-            startWith(null),
-            map((champion: string | null) => champion ? this._filter(champion) : this.listOfChampions.slice()));
-        })
+        if (!userDetails || !userDetails.id || userDetails.id == '') {
+          this.matSnackBar.open('Oops! You are not logged in. Please navigate back to the home screen and log in.', 'X', {duration: 5000})
+        } else {
+          this.userDetails = userDetails;
+          this.clashBotService.getUserDetails(userDetails.id)
+            .pipe(take(1),
+              timeout(4000),
+              catchError((err) => {
+                console.error(err);
+                this.matSnackBar.open('Oops! Failed to retrieve your User Information. Please try again later.', 'X', {duration: 5000});
+                return throwError(err);
+              }))
+            .subscribe((data: ClashBotUserDetails) => {
+              if (!data || !data.id) {
+                data.serverName = guilds[0].name;
+                data.preferredChampions = [];
+                data.subscriptions = {
+                  UpcomingClashTournamentDiscordDM: false
+                };
+              }
+              this.defaultGuild = data.serverName;
+              let preferredChampions = Array.isArray(data.preferredChampions) ? data.preferredChampions : [];
+              this.riotDdragonService.getListOfChampions()
+                .pipe(take(1),
+                  timeout(4000),
+                  catchError((err) => {
+                    console.error(err);
+                    this.matSnackBar.open('Oops! Failed to retrieve League Champion names. Please try again later.', 'X', {duration: 5000});
+                    return throwError(err);
+                  })).subscribe((championData) => {
+                this.listOfChampions = Object.keys(championData.data);
+                this.listOfChampions = this.listOfChampions.filter(record => !preferredChampions.includes(record));
+                this.initialAutoCompleteArray = JSON.parse(JSON.stringify(this.listOfChampions));
+                this.userDetailsForm = new FormGroup({
+                  preferredChampionsFC: new FormControl([...preferredChampions]),
+                  subscribedDiscordDMFC: new FormControl(data.subscriptions.UpcomingClashTournamentDiscordDM),
+                  defaultGuildFC: new FormControl(this.defaultGuild)
+                });
+                this.preferredChampions = new Set<string>(data.preferredChampions);
+                this.initialFormControlState = JSON.parse(JSON.stringify(this.userDetailsForm.value));
+                this.championsAutofillArray = this.championAutoCompleteCtrl.valueChanges.pipe(
+                  startWith(null),
+                  map((champion: string | null) => champion ? this._filter(champion) : this.listOfChampions.slice()));
+              })
+            })
+        }
       })
     })
   }
@@ -119,7 +152,6 @@ export class UserProfileComponent implements OnInit {
   }
 
   resetState() {
-    console.log(`${JSON.stringify(this.initialFormControlState)}`);
     this.userDetailsForm?.reset(this.initialFormControlState);
     this.preferredChampions = new Set<string>(this.initialFormControlState.preferredChampionsFC);
     this.listOfChampions = JSON.parse(JSON.stringify(this.initialAutoCompleteArray));
@@ -134,8 +166,9 @@ export class UserProfileComponent implements OnInit {
         .pipe(timeout(4000),
           catchError((err) => {
             console.error(err);
+            this.matSnackBar.open('Oops! Failed to persist your requested update. Please try again.', 'X', {duration: 5000});
             return throwError(err);
-        }))
+          }))
         .subscribe(() => {
           this.initialFormControlState = JSON.parse(JSON.stringify(this.userDetailsForm?.value));
           this.userDetailsForm?.markAsPristine();
@@ -148,7 +181,8 @@ export class UserProfileComponent implements OnInit {
   }
 
   compareArray(arr1: any[], arr2: any[]): boolean {
-    if (arr1.length === arr2.length) {
+    if ((Array.isArray(arr1) && Array.isArray(arr2))
+      && arr1.length === arr2.length) {
       for (let value of arr2) {
         if (!arr1.includes(value)) {
           return false;
