1cda8a3dc080fffadf080dcc2878e7f9791808f7
Daniel Poss <dpatposs@msn.com>
2021-08-31 12:14:52 -0500

    - Fixing issue upon removal of person from tentative queue

--- ClashBot-Service/service/clash-tentative-service-impl.js
+++ ClashBot-Service/service/clash-tentative-service-impl.js
@@ -5,17 +5,20 @@ const clashSubscriptionDbImpl = require('../dao/clash-subscription-db-impl');
 class ClashTentativeServiceImpl {
 
     async mapToApiResponse(tentativeDbResponse) {
+        let mappedApiResponse = {
+            serverName: tentativeDbResponse.serverName,
+            tournamentDetails: tentativeDbResponse.tournamentDetails
+        };
+        let idToPlayerNameMap = {};
         if (Array.isArray(tentativeDbResponse.tentativePlayers) && tentativeDbResponse.tentativePlayers.length > 0) {
-            const idToPlayerNameMap = await clashSubscriptionDbImpl.retrievePlayerNames(tentativeDbResponse.tentativePlayers);
-            return {
-                serverName: tentativeDbResponse.serverName,
-                tournamentDetails: tentativeDbResponse.tournamentDetails,
-                tentativePlayers: Array.isArray(tentativeDbResponse.tentativePlayers) ? tentativeDbResponse.tentativePlayers.map(data => {
-                    return !idToPlayerNameMap[data] ? data : idToPlayerNameMap[data]
-                }) : []
-            };
+            idToPlayerNameMap = await clashSubscriptionDbImpl.retrievePlayerNames(tentativeDbResponse.tentativePlayers);
         }
+        mappedApiResponse.tentativePlayers = Array.isArray(tentativeDbResponse.tentativePlayers) ? tentativeDbResponse.tentativePlayers.map(data => {
+            return !idToPlayerNameMap[data] ? data : idToPlayerNameMap[data]
+        }) : [];
+        return mappedApiResponse;
     }
+
 
     handleTentativeRequest(id, serverName, tournamentName, tournamentDay) {
         return new Promise(resolve => {
@@ -27,7 +30,10 @@ class ClashTentativeServiceImpl {
                     if (!isTentativeResults.onTentative) {
                         clashTeamsServiceImpl.unregisterFromTeam(id, serverName, tournamentName, tournamentDay)
                             .then(() => {
-                                clashTentativeDbImpl.addToTentative(id, serverName, {tournamentName: tournamentName, tournamentDay: tournamentDay}, isTentativeResults.tentativeList)
+                                clashTentativeDbImpl.addToTentative(id, serverName, {
+                                    tournamentName: tournamentName,
+                                    tournamentDay: tournamentDay
+                                }, isTentativeResults.tentativeList)
                                     .then((addTentativeDbResponse) => {
                                         resolve(this.mapToApiResponse(addTentativeDbResponse))
                                     });
