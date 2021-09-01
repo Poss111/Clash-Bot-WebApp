267773067e29caa2b9dcbcdcad0dace05b1b2b82
Daniel Poss <dpatposs@msn.com>
2021-09-01 15:06:02 -0500

    Service - Added empty object check for verify User service

--- ClashBot-Service/service/clash-user-service-impl.js
+++ ClashBot-Service/service/clash-user-service-impl.js
@@ -5,7 +5,7 @@ class ClashUserServiceImpl {
     checkIfIdExists(id, username, serverName) {
         return new Promise(resolve => {
             clashSubscriptionDbImpl.retrieveUserDetails(id).then((userDetails) => {
-                if (!userDetails) {
+                if (!userDetails || !userDetails.id) {
                     clashSubscriptionDbImpl.createUpdateUserDetails(id, serverName, username, [])
                         .then((createdUser) => {
                             console.log(`Created a new User entry for ('${id}')`)
--- ClashBot-Service/service/tests/clash-user-service-impl.unit.test.js
+++ ClashBot-Service/service/tests/clash-user-service-impl.unit.test.js
@@ -32,7 +32,29 @@ describe('Clash User Service Impl', () => {
             })
         })
 
-        test('User exists - It if is true, then return immediately.', () => {
+        test('User does not exist - It if is false (Is empty object), then create a base User Object.', () => {
+            const expectedUserId = '1';
+            const expectedUsername = 'Roidrage';
+            const expectedServername = 'Goon Squad';
+            const expectedPreferredChampions = [];
+            const expectedUserDetails = {
+                id: expectedUserId,
+                username: expectedUsername,
+                serverName: expectedServername,
+                preferredChampions: expectedPreferredChampions,
+            };
+            clashSubscriptionDbImpl.retrieveUserDetails.mockResolvedValue({});
+            clashSubscriptionDbImpl.createUpdateUserDetails.mockResolvedValue(expectedUserDetails);
+            return clashUserServiceImpl.checkIfIdExists(expectedUserId, expectedUsername, expectedServername).then((userDetails) => {
+                expect(clashSubscriptionDbImpl.retrieveUserDetails).toHaveBeenCalledTimes(1);
+                expect(clashSubscriptionDbImpl.retrieveUserDetails).toHaveBeenCalledWith(expectedUserId);
+                expect(clashSubscriptionDbImpl.createUpdateUserDetails).toHaveBeenCalledTimes(1)
+                expect(clashSubscriptionDbImpl.createUpdateUserDetails).toHaveBeenCalledWith(expectedUserId, expectedServername, expectedUsername, expectedPreferredChampions);
+                expect(userDetails).toEqual(expectedUserDetails);
+            })
+        })
+
+        test('User exists - It if is true , then return immediately.', () => {
             const expectedUserId = '1';
             const expectedUsername = 'Roidrage';
             const expectedServername = 'Goon Squad';
