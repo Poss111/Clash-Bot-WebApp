4f5901643ca7ab3a7a81048960751fd36f6ca9e5
Poss111 <dpatposs@msn.com>
2021-09-04 12:29:33 -0500

    - Updated formatting of date persisted in integration tests - Updated format to match more normal date time for Upcoming Tournaments card - Should format to expected timezone - Updated main title format

--- ClashBot-UI/src/app/upcoming-tournament-details-card/upcoming-tournament-details-card.component.ts
+++ ClashBot-UI/src/app/upcoming-tournament-details-card/upcoming-tournament-details-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {ClashTournaments} from "../interfaces/clash-tournaments";
 
 @Component({
@@ -6,29 +6,14 @@ import {ClashTournaments} from "../interfaces/clash-tournaments";
   templateUrl: './upcoming-tournament-details-card.component.html',
   styleUrls: ['./upcoming-tournament-details-card.component.scss']
 })
-export class UpcomingTournamentDetailsCardComponent implements OnInit {
+export class UpcomingTournamentDetailsCardComponent {
 
   @Input()
-  tournaments: ClashTournaments[] = [
-    {
-      tournamentName: 'awesome_sauce',
-      tournamentDay: '1',
-      startTime: 'September 04 2021 07:00 pm PDT',
-      registrationTime: 'September 14 2021 12:58 am PDT'
-    }
-  ];
-  currentDate?: Date;
-  dateFormat: string = 'MMM d, y h:mm:ss a z';
+  tournaments: ClashTournaments[] = [];
+  currentDate?: Date = new Date();
+  dateFormat: string = 'MMM d, y h:mm a';
   timezoneOffset: string = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-  constructor() {
-  }
-
-  ngOnInit(): void {
-    this.currentDate = new Date();
-    console.log(new Date(this.tournaments[0].startTime).toUTCString());
-    console.log(new Date(this.tournaments[0].startTime).getTimezoneOffset());
-    console.log(Intl.DateTimeFormat().resolvedOptions().timeZone);
-  }
+  constructor() {}
 
 }
