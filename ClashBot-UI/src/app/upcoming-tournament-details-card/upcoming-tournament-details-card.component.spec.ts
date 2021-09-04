5f9c22224cb22d6e780398e66ec7a49ecaf5b339
Poss111 <dpatposs@msn.com>
2021-09-04 14:04:42 -0500

    - Fixing failing test cases

--- ClashBot-UI/src/app/upcoming-tournament-details-card/upcoming-tournament-details-card.component.spec.ts
+++ ClashBot-UI/src/app/upcoming-tournament-details-card/upcoming-tournament-details-card.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UpcomingTournamentDetailsCardComponent } from './upcoming-tournament-details-card.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatListModule} from "@angular/material/list";
+import {TournamentNameTransformerPipe} from "../tournament-name-transformer.pipe";
+import {MatIconModule} from "@angular/material/icon";
 
 describe('UpcomingTournamentDetailsCardComponent', () => {
   let component: UpcomingTournamentDetailsCardComponent;
@@ -8,7 +11,8 @@ describe('UpcomingTournamentDetailsCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UpcomingTournamentDetailsCardComponent ]
+      declarations: [ UpcomingTournamentDetailsCardComponent, TournamentNameTransformerPipe ],
+      imports: [MatCardModule, MatListModule, MatIconModule]
     })
     .compileComponents();
   });
