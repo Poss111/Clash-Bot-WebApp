7581d9bd446980415254130f269972cb2e33d5f7
Poss111 <dpatposs@msn.com>
2022-04-17 20:57:49 -0500

    - Service - Adding in a test case to add in user event to be tracked

--- ClashBot-Service/dao/tests/clash-user-events-db-impl.unit.test.js
+++ ClashBot-Service/dao/tests/clash-user-events-db-impl.unit.test.js
@@ -10,29 +10,38 @@ beforeEach(() => {
     jest.resetAllMocks();
 })
 
-describe('Initialize Table connection', () => {
-    test('Initialize the table connection to be used.', async () => {
-        let expectedTableDef = {
-            hashKey: 'key',
-            timestamps: true,
-            schema: {
-                key: Joi.string(),
-                eventTime: Joi.string(),
-                eventName: Joi.string(),
-                eventDetails: Joi.string(),
-                username: Joi.string()
-            }
-        };
-        dynamoDbHelper.initialize = jest.fn().mockResolvedValue(expectedTableDef);
-        return clashUserEventsDbImpl.initialize().then(() => {
-            expect(clashUserEventsDbImpl.clashUserEventsTable).toEqual(expectedTableDef);
-            expect(dynamoDbHelper.initialize).toBeCalledWith(clashUserEventsDbImpl.tableName, expectedTableDef);
-        });
+describe('Clash User Events Clash Database Impl', () => {
+
+    describe('Initialize Table connection', () => {
+        test('Initialize the table connection to be used.', async () => {
+            let expectedTableDef = {
+                hashKey: 'key',
+                timestamps: true,
+                schema: {
+                    key: Joi.string(),
+                    eventTime: Joi.string(),
+                    eventName: Joi.string(),
+                    eventDetails: Joi.string(),
+                    username: Joi.string()
+                }
+            };
+            dynamoDbHelper.initialize = jest.fn().mockResolvedValue(expectedTableDef);
+            return clashUserEventsDbImpl.initialize().then(() => {
+                expect(clashUserEventsDbImpl.clashUserEventsTable).toEqual(expectedTableDef);
+                expect(dynamoDbHelper.initialize).toBeCalledWith(clashUserEventsDbImpl.tableName, expectedTableDef);
+            });
+        })
+
+        test('Error should be handled if it occurs during table initialization', async () => {
+            const expectedError = new Error('Failed to compile table def');
+            dynamoDbHelper.initialize = jest.fn().mockRejectedValue(expectedError);
+            return clashUserEventsDbImpl.initialize('Sample Table', {}).catch(err => expect(err).toEqual(expectedError));
+        })
     })
 
-    test('Error should be handled if it occurs during table initialization', async () => {
-        const expectedError = new Error('Failed to compile table def');
-        dynamoDbHelper.initialize = jest.fn().mockRejectedValue(expectedError);
-        return clashUserEventsDbImpl.initialize('Sample Table', {}).catch(err => expect(err).toEqual(expectedError));
+    describe('Add User Event', () => {
+        test('An event should be passed with event id, user id, and a base event detail.', () => {
+
+        })
     })
 })
