d8fa72b71f96dcb4aa78bab32ddc07648aa177e9
Poss111 <dpatposs@msn.com>
2022-05-01 13:56:05 -0500

    - UI - Adding a story for the upcoming tournaments card

---
+++ ClashBot-UI/src/app/upcoming-tournament-details-card/upcoming-tournament-details.stories.ts
@@ -0,0 +1,58 @@
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import { moduleMetadata } from '@storybook/angular';
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {UpcomingTournamentDetailsCardComponent} from "./upcoming-tournament-details-card.component";
+import {ClashTournaments} from "../interfaces/clash-tournaments";
+import {TournamentNameTransformerPipe} from "../tournament-name-transformer.pipe";
+import {MatListModule} from "@angular/material/list";
+
+// @ts-ignore
+export default {
+    title: 'Dashboard/UpcomingTournamentDetailsCard',
+    component: UpcomingTournamentDetailsCardComponent,
+    // More on argTypes: https://storybook.js.org/docs/angular/api/argtypes
+    argTypes: {
+        tournaments: { control: { type: ClashTournaments } },
+    },
+    decorators: [
+        moduleMetadata({
+            declarations: [TournamentNameTransformerPipe],
+            imports: [MatCardModule, MatIconModule, MatListModule],
+        })
+    ]
+} as Meta;
+
+const Template: Story<UpcomingTournamentDetailsCardComponent> = (args: UpcomingTournamentDetailsCardComponent) => ({
+    props: args,
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+    tournaments: [
+        {
+            tournamentName: 'awesome_sauce',
+            tournamentDay: '1',
+            startTime: new Date().toISOString(),
+            registrationTime: new Date().toISOString()
+        },
+        {
+            tournamentName: 'awesome_sauce',
+            tournamentDay: '2',
+            startTime: new Date().toISOString(),
+            registrationTime: new Date().toISOString()
+        },
+        {
+            tournamentName: 'awesome_sauce',
+            tournamentDay: '3',
+            startTime: new Date().toISOString(),
+            registrationTime: new Date().toISOString()
+        },
+        {
+            tournamentName: 'awesome_sauce',
+            tournamentDay: '4',
+            startTime: new Date().toISOString(),
+            registrationTime: new Date().toISOString()
+        }
+    ]
+};
