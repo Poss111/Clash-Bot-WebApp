c1a68f70cef2d4e41dce0a8266c23169ba68bb8d
Poss111 <dpatposs@msn.com>
2022-05-03 23:01:52 -0500

    - UI - Added dynamic version for riot ddragon api

--- ClashBot-UI/src/app/services/riot-ddragon.service.spec.ts
+++ ClashBot-UI/src/app/services/riot-ddragon.service.spec.ts
@@ -1,27 +1,71 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { RiotDdragonService } from './riot-ddragon.service';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {RiotDdragonService} from './riot-ddragon.service';
+import {HttpClientModule} from "@angular/common/http";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {DiscordService} from "./discord.service";
 
 describe('RiotDdragonService', () => {
-  let service: RiotDdragonService;
+    let service: RiotDdragonService;
+    let httpMock: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientModule]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [RiotDdragonService]
+        });
+        service = TestBed.inject(RiotDdragonService);
+        httpMock = TestBed.inject(HttpTestingController);
     });
-    service = TestBed.inject(RiotDdragonService);
-  });
 
-  test('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-
-  test('Should make a call to retrieve the list of valid champion names.', (done) => {
-    service.getListOfChampions().subscribe((data) => {
-      console.log(Object.keys(data.data));
-      expect(data).toBeTruthy();
-      done();
+    test('should be created', (done) => {
+        let mockVersions = [
+            "12.4.1",
+            "12.3.1",
+            "12.1.1",
+            "11.21.1",
+            "12.6.1",
+            "12.8.1",
+            "12.7.1",
+            "12.5.1",
+            "12.2.1",
+            "11.24.1",
+            "11.23.1",
+            "11.22.1"
+        ];
+        const request = httpMock.expectOne('https://ddragon.leagueoflegends.com/api/versions.json');
+        expect(request.request.method).toEqual('GET');
+        request.flush(mockVersions);
+        setTimeout(() => {
+            expect(service).toBeTruthy();
+            expect(service.host).toEqual('https://ddragon.leagueoflegends.com/cdn/12.8.1');
+            done();
+        })
     });
-  })
+
+    test('Should make a call to retrieve the list of valid champion names.', (done) => {
+        service.host = 'https://ddragon.leagueoflegends.com/cdn/12.8.1';
+        const mockChampionsList = {
+            type: "champion",
+            format: "standAloneComplex",
+            version: "12.8.1",
+            data: {
+                Aatrox: {
+                    version: "12.8.1",
+                    id: "Aatrox",
+                    key: "266",
+                    name: "Aatrox",
+                    title: "the Darkin Blade"
+                }
+            }
+        };
+        service.getListOfChampions().subscribe((data) => {
+            expect(data).toBeTruthy();
+            done();
+        });
+        const request = httpMock.expectOne('https://ddragon.leagueoflegends.com/cdn/12.8.1/data/en_US/champion.json');
+        expect(request.request.method).toEqual('GET');
+        request.flush(mockChampionsList);
+    })
+
 });
--- ClashBot-UI/src/app/services/riot-ddragon.service.ts
+++ ClashBot-UI/src/app/services/riot-ddragon.service.ts
@@ -2,17 +2,27 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {ChampionData} from "../interfaces/championData";
+import {take} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RiotDdragonService {
 
-  host: string = 'https://ddragon.leagueoflegends.com/cdn/12.2.1/data/en_US';
+  baseHost: string = 'https://ddragon.leagueoflegends.com'
+  host?: string;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+      if (!this.host) {
+        this.httpClient.get<string[]>(`${this.baseHost}/api/versions.json`)
+          .pipe(take(1))
+          .subscribe((versions) => {
+              this.host = `${this.baseHost}/cdn/${versions.sort().pop()}`
+          });
+      }
+  }
 
   getListOfChampions(): Observable<ChampionData> {
-      return this.httpClient.get<ChampionData>(`${this.host}/champion.json`);
+      return this.httpClient.get<ChampionData>(`${this.host}/data/en_US/champion.json`);
   }
 }
