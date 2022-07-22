e7ba029fdca48bae014f3082026cbf8ad3bec609
Poss111 <dpatposs@msn.com>
2022-07-22 00:01:56 -0500

    - Removing unused loggers and fixing formatting and importing for integration setup

--- Clash-Bot-Service-TS/integration/integration-setup.js
+++ Clash-Bot-Service-TS/integration/integration-setup.js
@@ -1,5 +1,5 @@
-let dynamoDbUtility = require('./dynamo-db-utility.setup');
-const logger = require('pino')();
+const dynamoDbUtility = require('./dynamo-db-utility.setup');
+const logger = require('../logger');
 
 process.env.INTEGRATION_TEST = true;
 process.env.REGION = 'us-east-1';
@@ -9,29 +9,29 @@ let clashTableData = new Map();
 delete process.env.LOCAL;
 delete process.env.TOKEN;
 
-let promise = new Promise((resolve, reject) => {
-    const timer = setTimeout(() => {
-        reject(new Error('Failed to load db data in set time.'))
-    }, 28000);
+const promise = new Promise((resolve, reject) => {
+  const timer = setTimeout(() => {
+    reject(new Error('Failed to load db data in set time.'));
+  }, 28000);
 
-    dynamoDbUtility.loadAllTables()
-        .then(data => {
-            logger.info('Table data setup successfully.');
-            clashTableData = data;
-            clearTimeout(timer);
-            resolve(data);
-        }).catch(err => {
-        clearTimeout(timer);
-        logger.error('Failed to setup data', err);
-        reject(err);
+  dynamoDbUtility.loadAllTables()
+    .then((data) => {
+      logger.info('Table data setup successfully.');
+      clashTableData = data;
+      clearTimeout(timer);
+      resolve(data);
+    }).catch((err) => {
+      clearTimeout(timer);
+      logger.error('Failed to setup data', err);
+      reject(err);
     });
 });
 promise.then(() => {
-    logger.info('Successfully setup table data.');
-    clashTableData.forEach((value, key) => {
-        logger.info(`${key} => ${JSON.stringify(value)}`);
-    });
+  logger.info('Successfully setup table data.');
+  clashTableData.forEach((value, key) => {
+    logger.info(`${key} => ${JSON.stringify(value)}`);
+  });
 }).catch((err) => {
-    logger.error('Failed to load DB data for setup.', err);
-    process.exit(1);
+  logger.error('Failed to load DB data for setup.', err);
+  process.exit(1);
 });
