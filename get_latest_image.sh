f3846ff3dd05f89864d00bdf276f37818c8ec0bb
Poss111 <dpatposs@msn.com>
2022-08-15 16:27:23 -0500

    - Fixing item for get latest image

--- get_latest_image.sh
+++ get_latest_image.sh
@@ -2,5 +2,5 @@
 echo "Running..."
 output=$(aws ecr describe-images --repository-name $1 --query 'sort_by(imageDetails,& imagePushedAt)[-1]')
 echo "Timestamp for image: $(echo $output | jq -r '.imagePushedAt')"
-echo "$2=$ECR_REGISTRY/$ECR_REPOSITORY:$(echo $output | jq -r '.imageTags[0]')" >> $GITHUB_ENV
+echo "$2=$ECR_REGISTRY/$1:$(echo $output | jq -r '.imageTags[0]')" >> $GITHUB_ENV
 echo "Finished"
