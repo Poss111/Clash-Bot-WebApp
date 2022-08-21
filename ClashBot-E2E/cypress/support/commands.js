63e59139be99748b4be8cc011d9ee98bc77c3aa4
Poss111 <dpatposs@msn.com>
2022-08-21 16:55:03 -0500

    - Adding in PR and deployment details for auth service

--- ClashBot-E2E/cypress/support/commands.js
+++ ClashBot-E2E/cypress/support/commands.js
@@ -5,28 +5,32 @@ Cypress.Commands.add('loginThroughOAuth', () => {
         const scope = Cypress.env('auth0_scope')
         const grant_type = Cypress.env('auth0_grant_type')
 
-        cy.request({
-            method: 'POST',
-            url: 'https://discord.com/api/oauth2/token',
-            auth: {
-                user: client_id,
-                pass: client_secret
-            },
-            body: {
-                'grant_type': grant_type,
-                'scope': scope
-            },
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        }).then(({body}) => {
-            window.localStorage.setItem('access_token', body.access_token);
-            window.localStorage.setItem('refresh_token', body.refresh_token);
-            window.localStorage.setItem('expires_at', `${Date.now() + 600000}`);
-            window.localStorage.setItem('access_token_stored_at', `${Date.now()}`);
-            window.localStorage.setItem('granted_scopes', '["identify","guilds"]');
-            cy.reload();
-        })
+        try {
+            cy.request({
+                method : 'POST',
+                url    : 'https://discord.com/api/oauth2/token',
+                auth   : {
+                    user: client_id,
+                    pass: client_secret
+                },
+                body   : {
+                    'grant_type': grant_type,
+                    'scope'     : scope
+                },
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }).then(({body}) => {
+                window.localStorage.setItem('access_token', body.access_token);
+                window.localStorage.setItem('refresh_token', body.refresh_token);
+                window.localStorage.setItem('expires_at', `${Date.now() + 600000}`);
+                window.localStorage.setItem('access_token_stored_at', `${Date.now()}`);
+                window.localStorage.setItem('granted_scopes', '["identify","guilds"]');
+                cy.reload();
+            })
+        } catch(error) {
+            throw new Error('Failed to login to Discord for Integration tests.')
+        }
     } else {
         cy.log('Already logged in.');
     }
--- check_diff_between_master.sh
+++ check_diff_between_master.sh
@@ -2,6 +2,7 @@
 echo "Running..."
 serviceUpdate=false
 wsServiceUpdate=false
+authServiceUpdate=false
 uiUpdate=false
 for i in $(git diff origin/master HEAD --name-only )
 do
@@ -13,14 +14,19 @@ do
   then
     wsServiceUpdate=true
   fi
+  if [[ "$i" == *"ClashBot-Auth-OpenAPI/"*  && !authServiceUpdate ]];
+  then
+    authServiceUpdate=true
+  fi
   if [[ "$i" == *"ClashBot-UI/"* && !$uiUpdate ]];
   then
     uiUpdate=true
   fi
 done
 
-echo "::set-output name=wsServiceUpdate::$wsServiceUpdate"
 echo "::set-output name=serviceUpdate::$serviceUpdate"
+echo "::set-output name=wsServiceUpdate::$wsServiceUpdate"
+echo "::set-output name=authServiceUpdate::$serviceUpdate"
 echo "::set-output name=uiUpdate::$uiUpdate"
 echo "Finished"
 
