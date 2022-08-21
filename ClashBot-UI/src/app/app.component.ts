19b0bc851b7005bd69d3587eb985d0ac3b3827a3
Poss111 <dpatposs@msn.com>
2022-08-20 22:21:02 -0500

    - Removing console output

--- ClashBot-UI/src/app/app.component.ts
+++ ClashBot-UI/src/app/app.component.ts
@@ -137,7 +137,6 @@ export class AppComponent implements OnInit, OnDestroy {
     }
 
     logUserOut() {
-        console.log("Log Out");
         this.oauthService.logOut();
         this.applicationDetailsService.logOutUser();
     }
