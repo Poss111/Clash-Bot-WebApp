c626f85e4c6487bf450ea16563e35af3979ab62b
Daniel Poss <dpatposs@msn.com>
2022-08-25 10:23:37 -0500

    - Adding in base app property to logger to services

--- ClashBot-Auth-OpenAPI/logger.js
+++ ClashBot-Auth-OpenAPI/logger.js
@@ -1,6 +1,8 @@
 const pino = require('pino');
 
 const level = process.env.LOGGER_LEVEL === undefined ? 'info' : process.env.LOGGER_LEVEL;
-const logger = pino({ level });
+const logger = pino({ level }).child({
+  app: 'ClashBot-Auth-OpenAPI',
+});
 
 module.exports = logger;
